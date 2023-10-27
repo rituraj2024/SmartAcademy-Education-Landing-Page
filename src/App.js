b7f74662844c7a5f16b197814d4b705b72d80a1c
rituraj2024 <45riturajprasad@gmail.com>
2023-10-27 12:33:27 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import {Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
+import About from "./pages/About";
+import Courses from "./pages/Courses";
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/courses" element={<Courses/>} />
+        {/* <Route path="/" element={<Home/>} /> */}
+        {/* <Route path="/" element={<Home/>} /> */}
       </Routes>
     </div>
   );
